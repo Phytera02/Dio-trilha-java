453f25e97508082d55317500ff838f91e12eca40
João Victor <joao.or02@outlook.com>
2024-07-23 15:07:38 -0300

    Atualização 23-07-24 15:08

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero = 1021;
+        String agencia = "MinhaAgencia";
+        String nomeCliente = "NomeCliente";
+        double saldo = 257.58;
+
+        System.out.println("Por favor, digite seu número:");
+        String numeroDigitado = scanner.nextLine();
+
+        System.out.println("Por favor, digite sua agência:");
+        String agenciaDigitada = scanner.nextLine();
+
+        System.out.println("Por favor informe seu nome");
+        String nomedoCliente = scanner.nextLine();
+
+        if (numeroDigitado.equals("1021")) {
+            String mensagemCliente = "Olá " + nomedoCliente + ", obrigado por ter criado uma conta em nosso banco," + " sua agência é " + agenciaDigitada + ", Sua conta " + numeroDigitado + ", seu saldo é R$ " + saldo;
+            System.out.println(mensagemCliente);
+        } else {
+            System.out.println("Número incorreto. Verifique seus dados.");
+        }
     }
 }
